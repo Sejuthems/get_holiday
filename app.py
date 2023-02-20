27246423c35fdee0289eb506664e01c87af3595f
Sejuthems <124249756+Sejuthems@users.noreply.github.com>
2023-02-20 19:14:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ col1, col2 = st.columns(2)
 with col1:
    st.header("START")
    d1 = st.date_input("Select START", datetime.date(2019, 7, 6))
-   st.write(type(d1))
+   st.write(d1)
 
 with col2:
    st.header("END")
